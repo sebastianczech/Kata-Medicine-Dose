0118079390a2b6f647daf17f7895df04827c59f2
Sebastian Czech <sebaczech@gmail.com>
2020-07-15 11:53:37 +0200

    test: Gdy ciśnienie spadnie poniżej 90, podaj 1 dawkę leku podnoszącego ciśnienie.

--- src/test/java/pl/pragmatists/trainings/medicinedosekata/MedicineDosingTest.java
+++ src/test/java/pl/pragmatists/trainings/medicinedosekata/MedicineDosingTest.java
@@ -1,13 +1,40 @@
 package pl.pragmatists.trainings.medicinedosekata;
 
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.mockito.junit.jupiter.MockitoExtension;
+import pl.pragmatists.trainings.medicinedosekata.dependencies.AlertService;
+import pl.pragmatists.trainings.medicinedosekata.dependencies.HealthMonitor;
+import pl.pragmatists.trainings.medicinedosekata.dependencies.MedicinePump;
 
+import static pl.pragmatists.trainings.medicinedosekata.dependencies.Medicine.PRESSURE_RAISING_MEDICINE;
+
+@ExtendWith(MockitoExtension.class)
 public class MedicineDosingTest {
 
-    @Test
-    public void __start__here() {
+    @InjectMocks
+    DoseController doseController;
 
+    @Mock
+    private HealthMonitor healthMonitor;
+
+    @Mock
+    private MedicinePump medicinePump;
+
+    @Mock
+    private AlertService alertService;
+
+    // Gdy ciśnienie spadnie poniżej 90, podaj 1 dawkę leku podnoszącego ciśnienie.
+    @Test
+    public void should_dose_medicine_when_pressure_lower_than_90() {
+        doseController.checkHealthAndApplyMedicine();
+
+        Mockito.verify(medicinePump).dose(PRESSURE_RAISING_MEDICINE);
     }
 
 }
